91bf6882efad45b02f3bf7ef2f3fe402ee07ade0
Enockkipkoech <enockkipkoech19@gmail.com>
2026-01-30 16:24:03 -0800

    :fire: App router, client-server components, Routing, data fetch :fire:

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,5 +1,6 @@
 
 const Analytics  = () => {
+    throw new Error("Analytics coming soon...")
     return (
         <div>
             <h1>About Fullstacks- Node5000 </h1>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import Hello from '@/components/hello'
+import {fetchRemoteData} from '@/components/fetchRemoteData'
 
-const Home = () => {
+const Home = async () => {
     console.log("Here we are here");
+    const data = await fetchRemoteData();
 
     return (
         <main>
         <div className={"text-5xl underline"}>Node 5000 Development Track - Fullstack </div>
         <Hello/>
+            <p>
+                {JSON.stringify(data, null, 2)}
+            </p>
+
         </main>
     )
 }
